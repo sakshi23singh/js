70e0dbb4cedf5e5c04c715aa5652f1fbfcc38efc
sakshi23singh <singh23sakshi10@gmail.com>
2025-05-21 15:37:38 +0000

    fdfg

---
+++ basics/num-maths.js
@@ -0,0 +1,36 @@
+// const score = 400
+// console.log(score); //400
+
+// const balance = new Number(100)
+// console.log(balance)//[Number:100]
+
+// console.log(balance.toString().length);//3
+// console.log(balance.toFixed(1));//100.0
+
+// const othernumber = 123.4778// 123
+// const othernumber = 123.6778//124
+
+// console.log(othernumber.toPrecision(3));
+
+// const hundreds = 1000000
+// console.log(hundreds.toLocaleString('en-IN'));
+// console.log(hundreds.toLocaleString());
+// *****************************************maths******************************************
+// console.log(Math)
+// console.log(Math.abs(-3));  //3
+// console.log(Math.round(567.657)); //568
+// console.log(Math.floor(567.657));//567
+ //console.log(Math.ceil(567.657)); //568
+// console.log(Math.min(2,3,4,56,67,1));
+// console.log(Math.max(2,3,4,56,67,1));
+
+
+
+// console.log(Math.random());
+// console.log(Math.floor(Math.random()*10)+1);
+
+const min = 10
+const max = 20
+
+console.log(Math.floor(Math.random()*(max-min+1))+min);
+
