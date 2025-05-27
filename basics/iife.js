622cbca3c88a7fb27e3e165f400ab700c9e5d01b
sakshi23singh <singh23sakshi10@gmail.com>
2025-05-27 16:52:37 +0000

    sdss

---
+++ basics/iife.js
@@ -0,0 +1,11 @@
+// iife= immediately invoked function expressions 
+//  jo function trnt call hojae isme important hai code end k bd semicolon lgana
+//()()
+( function chai(){
+    console.log("sakshi")
+})();
+  
+( (name ) => {
+    console.log("sakshi1")
+})();
+  
