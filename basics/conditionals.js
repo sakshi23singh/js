e3135552481e69fd13abbcc4ee2ae06e099cbac5
sakshi23singh <singh23sakshi10@gmail.com>
2025-05-29 14:45:07 +0000

    gfdfv

---
+++ basics/conditionals.js
@@ -0,0 +1,83 @@
+// if 
+// const IsUserLoggedIn = true
+// const temperature = 467
+
+// if (temperature<50){
+//     console.log("temperature is less than 50")
+// }
+// else {
+//     console.log("temperature is greater than 50")
+// }
+
+
+// const userLoggedIn = true
+// const debitcard = true
+// const loggedinfromgoogle = false
+// const loggedinfromemail = true  
+
+// if( userLoggedIn && debitcard ){
+//     console.log("Allow to buy course");
+    
+// }
+// if ( loggedinfromemail || loggedinfromgoogle){
+//     console.log("user logged in ");
+    
+// }
+
+// switch 
+//  const month = 3
+
+//  switch (month) {
+//     case 1:
+//         console.log("jan");
+//          break;
+//     case 2:
+//         console.log("feb")
+//         break;
+//     case 3:
+//         console.log("march");
+//         break;
+//     case 4:
+//         console.log("april");
+//         break;
+//     case 5:
+//         console.log("june");
+//         break;
+//     case 6:
+//         console.log("july");
+//         break;
+ 
+//     default:
+//         console.log("error")
+//         break;
+//  }
+
+// falsy values = false ,0,-0,bigint,0n,"",null,undefined,NaN
+// truthy value = "0","false"," ", [ ],{}, function(){}
+
+
+// const userEmail = " sakshi"
+// if (userEmail.length===0){
+//     console.log("array is empty"); 
+// }
+
+// const emptyobj = {
+//     1:"sakshi"
+// }
+// if (Object .keys(emptyobj).length===0){
+//     console.log("object is empty");
+    
+// }
+//nullish coalescing operatore (??): null undefined 
+//  let val1 ;
+//  //val1 = 5??10
+// //val1 = null??10
+// val1 = undefined??10
+
+//  console.log(val1)
+
+// terniary operator 
+// condition ? true: false 
+
+// const iceTeaPrice = 100 
+// iceTeaPrice<= 80? console.log("less than 80"): console.log("more than 80 ")
